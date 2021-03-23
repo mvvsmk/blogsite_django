975d83141c45902b352bb39c49ad78320b224ea3
M V V S Manoj Kumar <mkumar_bemba20@thapar.edu>
2021-03-23 07:15:31 +0530

    templates created

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
+posts = [
+    {
+      'author' : 'Manoj',
+      'title'  : 'Blog Post 1',
+      'content': 'First post content',
+      'date_posted': 'March 21, 2021'
+    },
+
+    {
+      'author' : 'Kakashi',
+      'title'  : 'Blog Post 2',
+      'content': 'Second post content',
+      'date_posted': 'March 21, 2021'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
