bfd808ea78f325bd359524277e1c4b3980c5efdd
M V V S Manoj Kumar <mkumar_bemba20@thapar.edu>
2021-03-24 18:30:40 +0530

    Database and Migrations implimented

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-      'author' : 'Manoj',
-      'title'  : 'Blog Post 1',
-      'content': 'First post content',
-      'date_posted': 'March 21, 2021'
-    },
-
-    {
-      'author' : 'Kakashi',
-      'title'  : 'Blog Post 2',
-      'content': 'Second post content',
-      'date_posted': 'March 21, 2021'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
