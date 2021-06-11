e6fdc60ac8d63e6874386dca329e0fcbb2252ca7
mvvsmk <mvvsmanojkumar@gmail.com>
2021-06-11 14:40:07 +0530

    5

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
+from .models import post
 
-
-posts = [
-    {
-      'author': 'Moturu' ,
-       'title':'Blog Post 1',
-       'content': 'First post content',
-       'date_posted': 'August 04, 2020'
-    },
-    {
-        'author': 'Polly' ,
-        'title':'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 27, 2018'
-    },
-]
 
 # Create your views here.
 
@@ -22,7 +8,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
