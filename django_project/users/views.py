9e0ce848bd0d9e14b536ad91590c75f7eb431f44
M V V S Manoj Kumar <mvvsmk@pop-os.localdomain>
2021-06-15 17:11:34 +0530

    login added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserResisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,11 +11,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username} ! ')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created ! You may now Log In ')
+            return redirect('login')
     
     
     else:
         form = UserResisterForm()
     
     return render(request,'users/register.html', {'form': form })
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
