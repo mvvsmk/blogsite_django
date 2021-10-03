e1d0e8f8b71d813f90e6e3a5df2b86b188f0b28b
@mvvsmk <mvvsmanojkumar@gmail.com>
2021-10-03 10:17:25 +0530

    post list view added, blog create added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import post
-
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -11,6 +17,35 @@ def home(request):
         'posts' : post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    model = post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
